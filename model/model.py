1cefe1fc0d9ec08876a1e31370d2a3f494221f18
ram <ramshlomor@gmail.com>
2021-02-15 12:52:27 +0200

    added Laboratory and test classes

--- model/model.py
+++ model/model.py
@@ -1,4 +1,5 @@
 import datetime
+
 class Address:
     def __init__(self,city:str,street:str,number:int):
         self.city=city
@@ -44,3 +45,30 @@ class Person:
     
     def set_infected_by(self,infected_by):
         self.infected_by=infected_by # instance of Person(self) type
+
+class Laboratory:
+    class_counter = 0
+    def __init__(self):
+        self.labId=Laboratory.class_counter
+        Laboratory.class_counter += 1
+    pass
+
+class Test:
+    class_counter = 0
+    def __init__(self, person:Person, lab:Laboratory, test_date:datetime.date, result_date:datetime.date=None, test_result:bool=None):
+        self.testId = Test.class_counter
+        Test.class_counter += 1
+        self.test_result=test_result
+        self.person = person
+        self.lab = lab
+        self.test_date = test_date
+        self.result_date = result_date
+        pass
+
+
+    def update_test_result(self, test_result:bool, result_date:datetime.date=datetime.datetime.now().date):
+        self.test_result = test_result
+        self.result_date = result_date
+        pass
+
+
